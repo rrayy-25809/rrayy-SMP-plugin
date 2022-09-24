a40fe8676d799c1afa525258f6c14239866adf44
rrayy <rrayy25809@gmail.com>
2022-09-24 12:52:54 +0900

    Changes to be committed:디렉토리 이름 변경 	renamed:    rrayySMP/src/main/java/com/rrayy/smp/cleaner/cleaner.java -> rrayySMP/src/main/java/com/rrayy/smp/function/cleaner.java 	modified:   rrayySMP/src/main/java/com/rrayy/smp/smp.java

---
+++ rrayySMP/src/main/java/com/rrayy/smp/function/cleaner.java
@@ -0,0 +1,54 @@
+package com.rrayy.smp.function;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.World;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+
+import com.rrayy.smp.smp;
+
+public class cleaner{
+    private smp plugin;
+    public cleaner(smp plugin) {
+        this.plugin = plugin;
+    }
+	public static int mob_clear(int ckm) {
+        int hkm = 0;
+        while (true) {
+            try {
+                for(Player alper : Bukkit.getOnlinePlayers()) {
+                    alper.sendMessage(ChatColor.GREEN+"2분 후, 모든 몹이 사라집니다");
+                }
+                TimeUnit.MINUTES.sleep(2);
+            } catch (InterruptedException e1) {
+                e1.printStackTrace();
+            }
+            
+            final List<World> worlds = (List<World>)Bukkit.getServer().getWorlds();
+            final int wdsize = worlds.size() - 1;
+            final World[] world = worlds.toArray(new World[wdsize]);
+            for (int i = 0; i <= wdsize; ++i) {
+                for (final Entity e : world[i].getEntities()) {
+                    if (e instanceof LivingEntity) {
+                        hkm = hkm + 1;
+                        e.remove();
+                    }
+                }
+            }
+            ckm = ckm + hkm;
+            for(Player alper : Bukkit.getOnlinePlayers()) {
+                alper.sendMessage(ChatColor.GREEN+"몹"+ChatColor.GOLD + Integer.toString(hkm)+ChatColor.GREEN+"마리가 삭제되었습니다.");
+            }
+            return ckm;
+        }
+    };
+        
+    public smp getPlugin() {
+        return plugin;
+    }
+}
--- rrayySMP/src/main/java/com/rrayy/smp/smp.java
+++ rrayySMP/src/main/java/com/rrayy/smp/smp.java
@@ -1,60 +1,33 @@
 package com.rrayy.smp;
 
-import java.util.ArrayList;
-
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.rrayy.smp.file_manage.text_manager;
-import com.rrayy.smp.cleaner.cleaner;
-
-public class smp extends JavaPlugin implements Listener {
-    @Getter private static smp instance;
-    ArrayList<String> get_item_list = new ArrayList<>();
-    int c_km;
-    int h_km;
-    boolean t = true;
+public class smp extends JavaPlugin{
     @Override
     public void onEnable() {     //플러그인 활성화 이벤트
-        instance = this;
         getLogger().info("SMP on");
-        get_item_list = text_list();
-        cleaner.mob_clear(3);
     }
 
     @Override
     public void onDisable(){    //플러그인 비활성화 이벤트
         System.out.println("플러그인이 종료됩니다.");
-        text_manager a = new text_manager(null);
-        a.save_file(get_item_list);
         getLogger().info(ChatColor.AQUA+"서버가 실행되는 동안"+ChatColor.GOLD + Integer.toString(5)+ChatColor.GREEN+"마리의 엔티티를 삭제하였습니다.");
     }
-
-    @EventHandler
-	public void onPlayerJoin(PlayerJoinEvent p) {     //플레이어가 게임에 접속했을 때
-        ((CommandSender) p).sendMessage("'/지급' 으로 기본 지급템을 받으세요"); //접속한 플레이어에게 명령어 사용법 알려주기
-		p.setJoinMessage(ChatColor.AQUA+"플레이어"+ChatColor.GREEN+ p +ChatColor.AQUA +"님께서"+ChatColor.DARK_GREEN +"라이 SMP"+ChatColor.AQUA +"에 입장했습니다!");
-	}
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (label.equalsIgnoreCase("지급")) {
-
-            if(t) {
+            /*if(false) {
                 sender.sendMessage(ChatColor.GREEN + "이미 기본지급 아이템을 받으셨습니다. 바꿀 수 없습니다.");
-            } else if (args.length > 0) {
+            } else */if (args.length > 0) {
                 if ("아머러".equalsIgnoreCase(args[0])) {
                     Player player = (Player)sender; //명령어 사용자 객체를 플레이어 객체로 변환할 수 있음
                     sender.sendMessage(ChatColor.GREEN + "기본지급 아이템을 '아머러' 로 정하셨습니다. 바꿀 수 없습니다.");
@@ -114,7 +87,7 @@ public class smp extends JavaPlugin implements Listener {
         return false;
     }
 
-    @EventHandler
+    /*@EventHandler
     public void onPlayerInteract(PlayerInteractEvent e) {   //플레이어가 상호작용을 할 때 실행
         //Player player = e.getPlayer();  //상호작용한 플레이어
         Action action = e.getAction();  //플레이어가 한 상호작용
@@ -129,9 +102,5 @@ public class smp extends JavaPlugin implements Listener {
                 }
             }
         }
-    }
-    public ArrayList<String> text_list(){
-        text_manager a = new text_manager(this);
-        return a.get_file(get_item_list);
-    }
+    }*/
 }
